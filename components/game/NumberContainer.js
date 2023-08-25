39e8ea477d6fce17d9b28c83de4d3de80b485c80
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-08-25 12:51:21 +0200

    ADD Styling
    Added styling for different screen sizes

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
 import Colors from '../../constants/colors';
 
@@ -12,11 +12,13 @@ function NumberContainer({ children }) {
 
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
+    padding: deviceWidth < 380 ? 12 : 24, //  On smaller screens the padding = 12 and on screens bigger than 380px the padding is 24
     margin: 24,
     borderRadius: 8,
     alignItems: 'center',
